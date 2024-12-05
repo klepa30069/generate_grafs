60eea772f4d70673dcc3f55080e41ac21ac66a62
seryakoff <seryakoff@internet.ru>
2024-12-05 10:56:27 +0300

    Add type notation and remove get metods

--- generate_solver_task9.py
+++ generate_solver_task9.py
@@ -7,19 +7,10 @@ class Generate_Solver_Task9:
     __solving_task: str
     __ans_task: int
 
-    __num_for_letter = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'H', 7: 'G', 8: 'K', 9: 'M', 10: 'N'}
+    __num_for_letter = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'H', 7: 'G', 8: 'K', 9: 'M', 10: 'N', 11: 'L'}
 
     def get_task(self) -> Generate_Solver:
         return Generate_Solver(self.__text_task, self.__matrix_task, self.__solving_task, self.__ans_task)
 
-    def __text_task(self) -> str:
-        pass
-
-    def __matrix_task(self) -> list[list[int]]:
-        pass
-
-    def __solving_task(self) -> str:
-        pass
-
-    def __ans_task(self) -> int:
+    def __init__(self) -> None:
         pass
