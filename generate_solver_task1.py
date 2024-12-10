4c98b129bfaa4071e2f9110f57fa79f7c1d37127
OverIQ429 <67504104+OverIQ429@users.noreply.github.com>
2024-12-10 10:34:36 +0300

    Вроде поправил

--- generate_solver_task1.py
+++ generate_solver_task1.py
@@ -1,35 +1,66 @@
-from turtledemo.penrose import start
-
-import networkx as nx
 import numpy as np
-import string
 from generate_solver import Generate_Solver
 import random
 from collections import Counter
 
 class Generate_Solver_Task1(Generate_Solver):
-    def __init__(self, type_task) -> None:
-        self.type_task = type_task
+
+    def __init__(self) -> None:
+        self.type_task = self.__type_task_generate
         self.points = ["A", "B", "C", "D", "E", "F", "G", "H"]
         self.num_letters = random.randint(6, 8)
-        self.letters = self.random_letter()
-        self.start_point, self.end_point = self.random_start_letter()
-        self.matrix = None
+        self.letters = self.__random_letter()
+        self.start_point, self.end_point = self.__random_start_letter()
+        self.matrix = self.__get_matrix_task()
         self.add_one = False
         self.Counts = None
-        self.anser = None
-        self.anser_type2 = None
+        self.anser = self.__get_ans_task()
+        self.text_task = self.__get_text_task()
+        self.solving_task = self.__get_solving_task()
 
-    def random_letter(self): #Рандомайзер букв в матрице
+    def get_task(self) -> Generate_Solver:
+        return Generate_Solver(self.text_task, self.matrix,
+                               self.letters, self.solving_task,
+                               self.anser)
+
+    def __get_text_task(self):
+        if self.type_task == 0:
+            self.text_task = ("На рисунке справа схема дорог Н-ского района изображена в виде графа, "
+                              "в таблице содержатся сведения о дорогах между населенными пунктами "
+                              "(звездочка означает, что дорога между соответствующими городами есть)."
+                              "Так как таблицу и схему рисовали независимо друг от друга, "
+                              "то нумерация населённых пунктов в таблице никак не связана с буквенными обозначениями на графе. "
+                              "Определите номера населенных пунктов {} и {} в таблице. "
+                              "В ответе запишите числа в порядке возрастания без разделителей.").format(self.letters[self.start_point], self.letters[self.end_point])
+        elif self.type_task == 1:
+            self.text_task = ("На рисунке схема дорог изображена в виде графа, "
+                              "в таблице содержатся сведения о длине этих дорог в километрах. "
+                              "Поскольку таблицу и схему рисовали независимо друг от друга, "
+                              "нумерация населённых пунктов в таблице никак не связана с буквенными обозначениями на графе. "
+                              "Определите длину дороги {}. "
+                              "В ответе запишите целое число— длину дороги в километрах.").format((self.letters[self.start_point], self.letters[self.end_point]))
+        elif self.type_task == 2:
+            self.text_task = ("На рисунке справа схема дорог Н-ского района изображена в виде графа, "
+                              "в таблице содержатся сведения о длинах этих дорог (в километрах)."
+                              "Так как таблицу и схему рисовали независимо друг от друга, "
+                              "то нумерация населённых пунктов в таблице никак не связана с буквенными обозначениями на графе. "
+                              "Определите, какова длина дороги из пункта {} в пункт {}. В ответе запишите целое число— так, как оно указано в таблице.").format(self.letters[self.start_point], self.letters[self.end_point])
+    def __type_task_generate(self) -> int:
+        numbers = [0, 1, 2]
+        weights = [0.25, 0.25, 0.5]
+        choice = random.choices(numbers, weights=weights, k=1)[0]
+        return choice
+
+    def __random_letter(self) -> dict: #Рандомайзер букв в матрице
         letters = random.sample(self.points, self.num_letters)  # Выбирает num_letters уникальных букв
         return dict(enumerate(letters))
 
-    def random_start_letter(self): #Рандомайзер стартовых буковок
+    def __random_start_letter(self) -> (int, int): #Рандомайзер стартовых буковок
         numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         point1, point2 = random.sample(numbers[:self.num_letters], 2)
         return point1, point2
 
-    def shuffle_adjacency_matrix(self, adjacency_matrix):
+    def __shuffle_adjacency_matrix(self, adjacency_matrix):
         """Перемешивает вершины в матрице смежности, используя NumPy."""
         adjacency_matrix = np.array(adjacency_matrix)
         num_nodes = len(adjacency_matrix)
@@ -39,10 +70,7 @@ class Generate_Solver_Task1(Generate_Solver):
 
         return shuffled_matrix
 
-    def get_text_task(self) -> str:
-        pass
-
-    def generate_random_matrix_with_edges(self, num_nodes, num_edges):
+    def __generate_random_matrix_with_edges(self, num_nodes, num_edges):
         """Генерирует случайную матрицу смежности с заданным количеством ребер."""
         adjacency_matrix = [[0] * num_nodes for _ in range(num_nodes)]
         edges_added = 0
@@ -63,8 +91,9 @@ class Generate_Solver_Task1(Generate_Solver):
                     adjacency_matrix[row][col] = rand_val
                     adjacency_matrix[col][row] = rand_val  # Для неориентированного графа
                     edges_added += 1
+
                     if row == self.start_point or row == self.end_point and col == self.start_point or col == self.end_point:
-                        self.anser_type2 = rand_val
+                        self.anser = rand_val
         if adjacency_matrix[self.start_point][self.end_point] == 0:
             if self.type_task == 0:
                 adjacency_matrix[self.start_point][self.end_point] = 1
@@ -73,10 +102,10 @@ class Generate_Solver_Task1(Generate_Solver):
                 rand_val = random.randint(10, 100)
                 adjacency_matrix[self.start_point][self.end_point] = rand_val
                 adjacency_matrix[self.end_point][self.start_point] = rand_val
-                self.anser_type2 = rand_val
+                self.anser = rand_val
         return adjacency_matrix
 
-    def add_node_list(self, adjacency_matrix):
+    def __add_node_list(self, adjacency_matrix):
         """Добавляет узел в матрицу смежности, представленную списком списков."""
         for row in adjacency_matrix:
             row.append(0)
@@ -93,10 +122,10 @@ class Generate_Solver_Task1(Generate_Solver):
             adjacency_matrix[random_val][num_nodes] = rand_val
         return adjacency_matrix
 
-    def add_counter(self, all_el: list):
+    def __add_counter(self, all_el: list):
         self.Counts = Counter(all_el)
 
-    def check_rows(self, matrix: list[list[int]]):
+    def __check_rows(self, matrix: list[list[int]]):
         all_el = []
         result = False
         for el in range(6):
@@ -104,32 +133,32 @@ class Generate_Solver_Task1(Generate_Solver):
             for deep_el in matrix[el]:
                 sum += deep_el
             all_el.append(sum)
-        self.add_counter(all_el)
+        self.__add_counter(all_el)
         for count in self.Counts.values():
             if count == 1:
                 result = True
         if result == False:
-            matrix = self.add_node_list(matrix)
+            matrix = self.__add_node_list(matrix)
             self.add_one = True
             self.letters[self.num_letters] = "I"
         return matrix
 
-    def get_matrix_task(self) -> list[list[int]]:
+    def __get_matrix_task(self) -> list[list[int]]:
         num_nodes = self.num_letters
         num_edges = num_nodes + num_nodes/2
-        adjacency_matrix = self.generate_random_matrix_with_edges(num_nodes, num_edges)
-        self.check_rows(adjacency_matrix)
+        adjacency_matrix = self.__generate_random_matrix_with_edges(num_nodes, num_edges)
+        self.__check_rows(adjacency_matrix)
         self.matrix = adjacency_matrix
-        self.TaskMatrix = self.shuffle_adjacency_matrix(adjacency_matrix)
+        self.TaskMatrix = self.__shuffle_adjacency_matrix(adjacency_matrix)
         return self.TaskMatrix
 
-    def get_solving_task(self, type_task: int) -> str:
+    def __get_solving_task(self) -> str:
         num_nodes = len(self.matrix)
         iterations = 0
         anser = f'Ответ: '
         startP = 0
         endP = 0
-        if type_task != 2:
+        if self.type_task != 2:
             for i in range(num_nodes):
                 iterations += 1
                 anser += "Вершина {} = П{}. ".format(self.anser[i], iterations)
@@ -142,24 +171,10 @@ class Generate_Solver_Task1(Generate_Solver):
                     startP = iterations
                 elif self.anser[i] == self.end_point:
                     endP = iterations
-            anser = "Вершина {} = П{}. Вершина {} = П{}. Расстояние между ними равно {}".format(start_letter, startP, end_letter, endP, self.anser_type2)
+            anser = "Вершина {} = П{}. Вершина {} = П{}. Расстояние между ними равно {}".format(start_letter, startP, end_letter, endP, self.anser)
         return anser
 
-    def get_ans_task(self, type_task: int) -> int:
-        if type_task == 0 or type_task == 1:
+    def __get_ans_task(self):
+        if self.type_task == 0 or self.type_task == 1:
             anser = (self.start_point + 1) * 10 + (self.end_point + 1)
-        elif type_task == 2:
-            anser = self.anser_type2
-        return anser
-
-Task1 = Generate_Solver_Task1(2)
-type_task = 2
-text = Task1.get_text_task()
-matrix = Task1.get_matrix_task()
-anser = Task1.get_ans_task(type_task)
-text_anser = Task1.get_solving_task(type_task)
-print(Task1.letters)
-print(anser)
-print(Task1.matrix)
-print(matrix)
-print(text_anser)
+            self.anser = anser
